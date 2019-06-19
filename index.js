c82b303f06f2716e9bd5805d7eac32b861001708
David Dobryakov <32088636+kantegory@users.noreply.github.com>
2019-06-19 21:38:57 +0300

    Update index.js
    add fetch, delete requestApi()

--- index.js
+++ index.js
@@ -11,18 +11,12 @@ function apiSearch(event) {
 	url = 'https://api.themoviedb.org/3/search/multi?api_key=' + token + '&language=' + language +'&query=' + searchText;
 	movies.innerHTML = 'Please, be patient! Downloading... :c';
 	
-
-	requestApi('GET', url)
+	fetch(url)
+		.then(function(value) {
+			return value.json();
+		})	
+		.then(function(output) {
 		
-		.then(function(request){
-		
-			try {
-				output = JSON.parse(request.responseText);
-			} catch (error) {
-				alert('Broken JSON... :c');
-				return;
-			}
-
 			let output_inner = '';
 
 			if (output.results.length == 0) {
@@ -34,7 +28,7 @@ function apiSearch(event) {
 				output.results.forEach( (film) => {
 				let filmName = film.name || film.title,
 				filmDate = film.first_air_date || film.release_date || 'No data... :c',
-				filmPic = film.poster_path ? `https://image.tmdb.org/t/p/w200/${film.poster_path}` : 'https://placekitten.com/200/300';
+				filmPic = film.poster_path ? `https://image.tmdb.org/t/p/w200/${film.poster_path}` : 'http://atlantaplanningguys.com/wp-content/uploads/2009/10/question-mark-image-200x300.jpg';
 				output_inner += `<a href="https://www.themoviedb.org/${film.media_type}/${film.id}" class="item col-12 col-sm-6 col-md-4 col-lg-3 text-center" title="${film.overview}" target="_blank">
 	                    <img src="${filmPic}"> <br/>
 	                    <b> ${filmName} </b> <span class="badge badge-primary"> ${film.vote_average} </span> <br/>
@@ -42,9 +36,7 @@ function apiSearch(event) {
 	                </a>`;
 
 			});
-			
-			};
-			
+		};	
 			movies.innerHTML = output_inner;
 		
 		})
@@ -57,30 +49,3 @@ function apiSearch(event) {
 
 searchForm.addEventListener('submit', apiSearch);
 searchForm.addEventListener('change', apiSearch);
-
-
-function requestApi(method, url) {
-	
-	return new Promise (function (resolve, reject) {
-
-		const request = new XMLHttpRequest();
-		request.open(method, url);
-		
-		request.addEventListener('load', function() {
-			if (request.status !== 200) {
-				reject({status: request.status});
-				return;
-			}
-
-			resolve(request);
-
-		});
-
-		request.addEventListener('error', function() {
-			reject({status: request.status});
-		});
-
-		request.send();
-
-	});
-};
